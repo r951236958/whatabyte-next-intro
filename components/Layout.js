81befcdce157f510655943555bc646bcdefefea7
Nick Lin <r951236958@gmail.com>
2020-11-11 16:45:31 +0000

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+// components/Layout.js
+import Head from "next/head";
+
+import Header from "./Header";
+import NavBar from "./NavBar";
+
+import "./Layout.scss";
+import "./index.scss";
+
+import navButtons from "../config/buttons";
+
+const Layout = props => {
+  const appTitle = `> WHATABYTE`;
+
+  return (
+    <div className="Layout">
+      <Head>
+        <title>WHATABYTE</title>
+      </Head>
+
+      <Header appTitle={appTitle} />
+      <div className="Content">{props.children}</div>
+      <NavBar navButtons={navButtons} />
+    </div>
+  );
+};
+
+export default Layout;
